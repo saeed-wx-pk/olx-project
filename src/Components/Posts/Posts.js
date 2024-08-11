8b63c49c2f8efa414b420484613bae15cd1de18b
Saeed pk <saeedpk781@gmail.com>
2024-08-11 16:13:05 +0530

    Some design changed

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -79,22 +79,30 @@ function Posts() {
           <span>Fresh recommendations</span>
         </div>
         <div className="cards">
-          <div className="card">
-            <div className="favorite">
-              <Heart></Heart>
-            </div>
-            <div className="image">
-              <img src="../../../Images/R15V3.jpg" alt="" />
-            </div>
-            <div className="content">
-              <p className="rate">&#x20B9; 250000</p>
-              <span className="kilometer">Two Wheeler</span>
-              <p className="name"> YAMAHA R15V3</p>
-            </div>
-            <div className="date">
-              <span>10/5/2021</span>
-            </div>
-          </div>
+          {
+            products.map((product)=>{
+                <div className="card"  onClick={()=>{
+                    setPostDetails(product)
+                    navigate('/view')
+                  }}>
+                  <div className="favorite">
+                    <Heart></Heart>
+                  </div>
+                  <div className="image">
+                    <img src={product.url} alt="product image" />
+                  </div>
+                  <div className="content">
+                    <p className="rate">&#x20B9; {product.price}</p>
+                    <span className="kilometer">{product.category}</span>
+                    <p className="name"> {product.name}</p>
+                  </div>
+                  <div className="date">
+                    <span>{product.createdAt}</span>
+                  </div>
+                </div>
+            })
+          }
+          
         </div>
       </div>
     </div>
